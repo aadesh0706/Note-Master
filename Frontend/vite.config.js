6fe3f708df2aee174c74095bea1fdbad7098adf7
Aadesh Gulumbe <71577243+aadesh0706@users.noreply.github.com>
2024-05-25 23:20:38 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://note-master-8g11.vercel.app/",
+      "/api": "https://note-master-8g11.vercel.app",
     },
   },
   plugins: [react()],
